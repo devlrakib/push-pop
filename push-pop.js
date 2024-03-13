ed2e5013fbb9aff47a689d0a78b7d89621d408c1
devlrakib <devrakib5@gmail.com>
2024-03-13 22:07:27 +0600

    first commit

---
+++ push-pop.js
@@ -0,0 +1,9 @@
+// Javascript Push 
+var numbers = [70, 45, 98, 50];
+numbers.push(60);
+console.log(numbers);
+
+// Javascript Pop 
+var friends =['Sakib', 'Tamim', 'Mushfiqur', 'Mashrafi'];
+friends.pop(2)
+console.log(friends);
